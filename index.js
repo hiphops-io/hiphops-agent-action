ee6cf0b06e4b79f9178a8c5ae0167162eef06435
manterfield <tommanterfield@gmail.com>
2022-01-09 00:39:35 +0000

    Added token input along with test release creation

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  console.log(`Testing dispatch payload`);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    console.log(`Testing dispatch payload`);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+
+    const token = core.getInput("token");
+    const octokit = github.getOctokit(token);
+    const response = await octokit.rest.repos.createRelease({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      tag_name: "v1.0.0",
+      target_commitish: "main",
+    });
+    console.log(response);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
