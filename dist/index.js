8afe47909d2d94daa26fe5c6dd8fbda3bee9ff79
manterfield <tommanterfield@gmail.com>
2022-01-11 11:17:00 +0000

    Added debug to be certain code has changed

--- dist/index.js
+++ dist/index.js
@@ -11507,31 +11507,24 @@ async function run() {
     console.log(`Testing dispatch payload`);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
 
-    console.log("Testing release creation");
     const token = core.getInput("token");
     const octokit = github.getOctokit(token);
 
+    console.log("--- 001");
     // Try to create the release, capture the error if not.
-    console.log("Creating release with error callback");
-    const response = await octokit.rest.repos.createRelease(
-      {
+    const response = await octokit.rest.repos
+      .createRelease({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         tag_name: "v1.0.0",
         target_commitish: "main",
-      },
-      function (err, res) {
-        if (err) {
-          console.log("Got an error!");
-          console.log(err);
-        } else {
-          console.log("Didn't get an error!");
-          console.log(JSON.stringify(res));
-        }
-      }
-    );
+      })
+      .catch((err) => {
+        console.log("Error thrown.");
+        console.log(err);
+      });
+
     console.log(response);
   } catch (error) {
     core.setFailed(error.message);
